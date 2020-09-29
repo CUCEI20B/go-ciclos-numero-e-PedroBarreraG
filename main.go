04674a831bf0b557a5c17af72e557c21bea3edff
Pedro Barrera <pedro.barrera5641@alumnos.udg.mx>
2020-09-29 14:43:36 -0500

    Prueba01

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var x float64
+	fmt.Scan(&x)
+	var e float64
+	e = 0
+	var op float64
+	var n2 float64
+	var n float64
+	n = 1
+	for n <= x {
+		op = 1
+		n2 = 1
+		for n2 <= n {
+			op = op * n2
+			n2 = n2 + 1
+		}
+		e = e + 1/op
+		n = n + 1
+	}
+	fmt.Println(e)
 }
